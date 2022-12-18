237030afe313d96a0a100870a706fe84d7eaa0ff
Batu Balcı <beligbatubalci@hotmail.com>
2022-12-18 16:47:53 +0300

    rename private fields by using 'm_' prefix

--- MeshData/mesh.cpp
+++ MeshData/mesh.cpp
@@ -2,59 +2,60 @@
 //------------------------------------------------------ Corner -----------------------------------------------------------
 MeshData::Domain::Corner::Corner(double x, double y)
 {
-	this->coordinates.xCoord = x;
-	this->coordinates.yCoord = y;
+	this->m_Coordinates.xCoord = x;
+	this->m_Coordinates.yCoord = y;
 }
-const DoublyConnectedList::Vertex::Coordinates& MeshData::Domain::Corner::getCoordinates() { return this->coordinates; }
+const DoublyConnectedList::Vertex::Coordinates& MeshData::Domain::Corner::getCoordinates() { return this->m_Coordinates; }
 
 //------------------------------------------------------ Edge -------------------------------------------------------------
 MeshData::Domain::Edge::Edge(std::shared_ptr<MeshData::Domain::Corner> start,
-	std::shared_ptr<MeshData::Domain::Corner> end) : startCorner(start), endCorner(end) 
+	std::shared_ptr<MeshData::Domain::Corner> end) : m_StartCorner(start), m_EndCorner(end)
 {
 	calculateLength();
 }
-std::shared_ptr<MeshData::Domain::Corner> MeshData::Domain::Edge::getStartCorner() { return this->startCorner; }
-std::shared_ptr<MeshData::Domain::Corner> MeshData::Domain::Edge::getEndCorner() { return this->endCorner; }
-double MeshData::Domain::Edge::getLength() { return this->length; }
-double MeshData::Domain::Edge::calculateLength() { this->length = sqrt(pow((endCorner->getCoordinates().xCoord - startCorner->getCoordinates().xCoord), 2)
-	+ pow((endCorner->getCoordinates().yCoord - startCorner->getCoordinates().yCoord), 2)); getLength(); }
+std::shared_ptr<MeshData::Domain::Corner> MeshData::Domain::Edge::getStartCorner() { return this->m_StartCorner; }
+std::shared_ptr<MeshData::Domain::Corner> MeshData::Domain::Edge::getEndCorner() { return this->m_EndCorner; }
+double MeshData::Domain::Edge::getLength() { return this->m_Length; }
+double MeshData::Domain::Edge::calculateLength() { this->m_Length = sqrt(pow((m_EndCorner->getCoordinates().xCoord - m_StartCorner->getCoordinates().xCoord), 2)
+	+ pow((m_EndCorner->getCoordinates().yCoord - m_StartCorner->getCoordinates().yCoord), 2)); getLength(); }
 
 //------------------------------------------------ Point Constriant -------------------------------------------------------
 MeshData::Domain::PointConstraint::PointConstraint(double x, double y)
 {
-	this->coordinates.xCoord = x;
-	this->coordinates.yCoord = y;
+	this->m_Coordinates.xCoord = x;
+	this->m_Coordinates.yCoord = y;
 }
-const DoublyConnectedList::Vertex::Coordinates& MeshData::Domain::PointConstraint::getCoordinates() { return this->coordinates; }
+const DoublyConnectedList::Vertex::Coordinates& MeshData::Domain::PointConstraint::getCoordinates() { return this->m_Coordinates; }
 
 //------------------------------------------------ Line Constriant --------------------------------------------------------
 MeshData::Domain::LineConstraint::LineConstraint(double startX, double startY, double endX, double endY)
 {
-	this->startCoord.xCoord = startX;
-	this->endCoord.xCoord = endX;
-	this->startCoord.yCoord = startY;
-	this->endCoord.yCoord = endY;
+	this->m_StartCoord.xCoord = startX;
+	this->m_EndCoord.xCoord = endX;
+	this->m_StartCoord.yCoord = startY;
+	this->m_EndCoord.yCoord = endY;
 }
-const DoublyConnectedList::Vertex::Coordinates& MeshData::Domain::LineConstraint::getStartCoordinates() { return this->startCoord; }
-const DoublyConnectedList::Vertex::Coordinates& MeshData::Domain::LineConstraint::getEndCoordinates() { return this->endCoord; }
+const DoublyConnectedList::Vertex::Coordinates& MeshData::Domain::LineConstraint::getStartCoordinates() { return this->m_StartCoord; }
+const DoublyConnectedList::Vertex::Coordinates& MeshData::Domain::LineConstraint::getEndCoordinates() { return this->m_EndCoord; }
 void MeshData::Domain::LineConstraint::addPointConstraint(std::shared_ptr<PointConstraint> pointConstraint) 
-{ this->pointConstraints.push_back(pointConstraint); }
+{ this->m_PointConstraints.push_back(pointConstraint); }
 const std::vector<std::shared_ptr<MeshData::Domain::PointConstraint>>& 
-MeshData::Domain::LineConstraint::getConstraints() { return this->pointConstraints; }
+MeshData::Domain::LineConstraint::getConstraints() { return this->m_PointConstraints; }
 
 //------------------------------------------------ Edge Constraint --------------------------------------------------------
-MeshData::Domain::EdgeConstraint::EdgeConstraint(std::shared_ptr<MeshData::Domain::Edge> edge) : edge(edge) {}
-std::shared_ptr<MeshData::Domain::Edge> MeshData::Domain::EdgeConstraint::getEdge() { return this->edge; }
+MeshData::Domain::EdgeConstraint::EdgeConstraint(std::shared_ptr<MeshData::Domain::Edge> edge) : m_Edge(edge) {}
+std::shared_ptr<MeshData::Domain::Edge> MeshData::Domain::EdgeConstraint::getEdge() { return this->m_Edge; }
 void MeshData::Domain::EdgeConstraint::addRelativeConstraintLocations(double relativeConstraintLocation)
-{ this->relativeConstraintLocations.push_back(relativeConstraintLocation); }
-const std::vector<double>& MeshData::Domain::EdgeConstraint::getRelativeConstraintLocations() { return this->relativeConstraintLocations; }
+{ this->m_RelativeConstraintLocations.push_back(relativeConstraintLocation); }
+const std::vector<double>& MeshData::Domain::EdgeConstraint::getRelativeConstraintLocations() { return this->m_RelativeConstraintLocations; }
 
 //------------------------------------------------------ Domain -----------------------------------------------------------
-void MeshData::Domain::addCorner(std::shared_ptr<Corner> corner) { this->corners.push_back(corner); }
-void MeshData::Domain::addPointConstriant(std::shared_ptr<PointConstraint> pointConstraint) { this->pointConstraints.push_back(pointConstraint); }
-void MeshData::Domain::addLineConstraint(std::shared_ptr<LineConstraint> lineConstraint) { this->lineConstraints.push_back(lineConstraint); }
-const std::vector<std::shared_ptr<MeshData::Domain::Corner>>& MeshData::Domain::getCorners() { return this->corners; }
-const std::vector<std::shared_ptr<MeshData::Domain::PointConstraint>>& MeshData::Domain::getPointConstriants() { return this->pointConstraints; }
-const std::vector<std::shared_ptr<MeshData::Domain::LineConstraint>>& MeshData::Domain::getLineConstraints() { return this->lineConstraints; }
-const std::vector<std::shared_ptr<MeshData::Domain::EdgeConstraint>>& MeshData::Domain::getEdgeConstraints() { return this->edgeConstraints; }
-std::shared_ptr<DoublyConnectedList::DCEL> MeshData::Domain::getDCEL() { return DCEL; }
+void MeshData::Domain::addCorner(std::shared_ptr<Corner> corner) { this->m_Corners.push_back(corner); }
+void MeshData::Domain::addPointConstriant(std::shared_ptr<PointConstraint> pointConstraint) { this->m_PointConstraints.push_back(pointConstraint); }
+void MeshData::Domain::addLineConstraint(std::shared_ptr<LineConstraint> lineConstraint) { this->m_LineConstraints.push_back(lineConstraint); }
+void MeshData::Domain::addEdgeConstraint(std::shared_ptr<EdgeConstraint> edgeConstraint) { this->m_EdgeConstraints.push_back(edgeConstraint); }
+const std::vector<std::shared_ptr<MeshData::Domain::Corner>>& MeshData::Domain::getCorners() { return this->m_Corners; }
+const std::vector<std::shared_ptr<MeshData::Domain::PointConstraint>>& MeshData::Domain::getPointConstriants() { return this->m_PointConstraints; }
+const std::vector<std::shared_ptr<MeshData::Domain::LineConstraint>>& MeshData::Domain::getLineConstraints() { return this->m_LineConstraints; }
+const std::vector<std::shared_ptr<MeshData::Domain::EdgeConstraint>>& MeshData::Domain::getEdgeConstraints() { return this->m_EdgeConstraints; }
+std::shared_ptr<DoublyConnectedList::DCEL> MeshData::Domain::getDCEL() { return m_DCEL; }
