01dca7d3e3eeb6ec7f1cbb9aa105828b5b44925f
Batu Balcı <beligbatubalci@hotmail.com>
2023-01-14 16:29:49 +0300

    This commit will track the changes of main funciton

--- EntryPoint.cpp
+++ EntryPoint.cpp
@@ -1,4 +1,5 @@
-#include "DoublyConnectedEdgeList.h"
+#include "mesh.h"
+typedef DoublyConnectedList::Vertex::Coordinates coord;
 int main()
 {
 	std::vector<std::vector<double>> verts = { {0.0 ,0.0}, {1.0 ,0.0} , {2.0 ,0.0} , {0.0 ,1.0}, {1.0 ,1.0} , {2.0 ,1.0} , {0.0 ,2.0}, {1.0 ,2.0} , {2.0 ,2.0} };
@@ -42,9 +43,45 @@ int main()
 	hEdges = a.getHalfEdges();
 	faces = a.getFaces();
 
-	auto res = a.findPoint(vertex);
+	std::vector<std::shared_ptr<MeshData::Domain>> domains;
+
+	auto domain = std::make_shared<MeshData::Domain>();
+	auto corner1 = std::make_shared<MeshData::Domain::Corner>(0.0, 0.0, -1.0, -1.0);
+	auto corner2 = std::make_shared<MeshData::Domain::Corner>(10.0, 0.0, 1.0, -1.0);
+	auto corner3 = std::make_shared<MeshData::Domain::Corner>(10.0, 10.0, 1.0, 1.0);
+	auto corner4 = std::make_shared<MeshData::Domain::Corner>(0, 10.0, -1.0, 1.0);
+	auto edge1 = std::make_shared<MeshData::Domain::Edge>(corner1, corner2);
+	auto edge2 = std::make_shared<MeshData::Domain::Edge>(corner2, corner3);
+	auto edge3 = std::make_shared<MeshData::Domain::Edge>(corner3, corner4);
+	auto edge4 = std::make_shared<MeshData::Domain::Edge>(corner4, corner1);
+	auto cons1 = std::make_shared<MeshData::Domain::EdgeConstraint>(std::vector<double>{0.2});
+	auto cons2 = std::make_shared<MeshData::Domain::EdgeConstraint>(std::vector<double>{ 0.3, 0.8 });
+	auto cons3 = std::make_shared<MeshData::Domain::EdgeConstraint>(std::vector<double>{ 0.3, 0.70 });
+	auto cons4 = std::make_shared<MeshData::Domain::EdgeConstraint>(std::vector<double>{ 0.5 });
+	edge1->addEdgeConstraint(cons1);
+	edge2->addEdgeConstraint(cons2);
+	edge3->addEdgeConstraint(cons3);
+	edge4->addEdgeConstraint(cons4);
+	domain->addCorner(corner1);
+	domain->addCorner(corner2);
+	domain->addCorner(corner3);
+	domain->addCorner(corner4);
+	domain->addEdge(edge1);
+	domain->addEdge(edge2);
+	domain->addEdge(edge3);
+	domain->addEdge(edge4);
+	//domain->addPointConstriant(std::make_shared<MeshData::Domain::PointConstraint>(4.65, 6.78));
+	//domain->addLineConstraint(std::make_shared<MeshData::Domain::LineConstraint>(0.6, 8.5, 7.65, 9.78));
+	domain->setEdgeLength(1.0);
+
+	domains.push_back(domain);
+
+	auto mesh = BasicQuadMesh();
+
+	mesh.Mesh(domains);
+	auto res = a.findPoint(vertex->getCoordinate());
 	std::shared_ptr<DoublyConnectedList::Vertex> close;
 	if(res)
-		close = res->getClosestPoint(vertex);
+		close = res->getClosestPoint(vertex->getCoordinate());
 	a.ExportVTKFormat("asd.vtk");
 }
