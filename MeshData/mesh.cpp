dda5c908a038f04621c8e78b7f1c6c96f9a3155d
Batu Balcı <beligbatubalci@hotmail.com>
2023-01-14 16:26:57 +0300

    Add new mesh algorithm which is called basic mesh (this commit will be updated)

--- MeshData/mesh.cpp
+++ MeshData/mesh.cpp
@@ -1,61 +1,78 @@
-#include "mesh.h"
-//------------------------------------------------------ Corner -----------------------------------------------------------
-MeshData::Domain::Corner::Corner(double x, double y)
+#include "Mesh.h"
+
+void BasicQuadMesh::Mesh(std::vector<MeshData::Domain>& domains)
 {
-	this->m_Coordinates.xCoord = x;
-	this->m_Coordinates.yCoord = y;
+	for (int i = 0; i < 5; i++)
+		if (this->TryMesh(domains, i))
+			break;
 }
-const DoublyConnectedList::Vertex::Coordinates& MeshData::Domain::Corner::getCoordinates() { return this->m_Coordinates; }
-
-//------------------------------------------------------ Edge -------------------------------------------------------------
-MeshData::Domain::Edge::Edge(std::shared_ptr<MeshData::Domain::Corner> start,
-	std::shared_ptr<MeshData::Domain::Corner> end) : m_StartCorner(start), m_EndCorner(end)
+bool BasicQuadMesh::TryMesh(std::vector<MeshData::Domain>& domains, int tryCount)
 {
-	calculateLength();
-}
-std::shared_ptr<MeshData::Domain::Corner> MeshData::Domain::Edge::getStartCorner() { return this->m_StartCorner; }
-std::shared_ptr<MeshData::Domain::Corner> MeshData::Domain::Edge::getEndCorner() { return this->m_EndCorner; }
-double MeshData::Domain::Edge::getLength() { return this->m_Length; }
-double MeshData::Domain::Edge::calculateLength() { this->m_Length = sqrt(pow((m_EndCorner->getCoordinates().xCoord - m_StartCorner->getCoordinates().xCoord), 2)
-	+ pow((m_EndCorner->getCoordinates().yCoord - m_StartCorner->getCoordinates().yCoord), 2)); getLength(); }
+	if (tryCount == 0)
+		this->m_OptimumLength = this->FindOptimumEdgeLength(domains);
+	else
+		this->m_OptimumLength /= 2;
 
-//------------------------------------------------ Point Constriant -------------------------------------------------------
-MeshData::Domain::PointConstraint::PointConstraint(double x, double y)
+	this->BuildInitialGrid(domains);
+	this->ReturnToCartesian(domains);
+	this->AddPointConstraintDomains(domains);
+	this->AddLineConstraint(domains);
+
+	return this->CheckQualityOfMesh(domains);
+}
+void BasicQuadMesh::BuildInitialGrid(std::vector<MeshData::Domain>& domains)
 {
-	this->m_Coordinates.xCoord = x;
-	this->m_Coordinates.yCoord = y;
-}
-const DoublyConnectedList::Vertex::Coordinates& MeshData::Domain::PointConstraint::getCoordinates() { return this->m_Coordinates; }
 
-//------------------------------------------------ Line Constriant --------------------------------------------------------
-MeshData::Domain::LineConstraint::LineConstraint(double startX, double startY, double endX, double endY)
+}
+void BasicQuadMesh::AddPointConstraintDomains(std::vector<MeshData::Domain>& domains)
 {
-	this->m_StartCoord.xCoord = startX;
-	this->m_EndCoord.xCoord = endX;
-	this->m_StartCoord.yCoord = startY;
-	this->m_EndCoord.yCoord = endY;
+	for (auto& domain : domains)
+		this->AddPointConstraint(domain, domain.getPointConstriants());
 }
-const DoublyConnectedList::Vertex::Coordinates& MeshData::Domain::LineConstraint::getStartCoordinates() { return this->m_StartCoord; }
-const DoublyConnectedList::Vertex::Coordinates& MeshData::Domain::LineConstraint::getEndCoordinates() { return this->m_EndCoord; }
-void MeshData::Domain::LineConstraint::addPointConstraint(std::shared_ptr<PointConstraint> pointConstraint) 
-{ this->m_PointConstraints.push_back(pointConstraint); }
-const std::vector<std::shared_ptr<MeshData::Domain::PointConstraint>>& 
-MeshData::Domain::LineConstraint::getConstraints() { return this->m_PointConstraints; }
+void BasicQuadMesh::AddPointConstraint(MeshData::Domain& domain, const std::vector<std::shared_ptr<MeshData::Domain::PointConstraint>>& constrains)
+{
+	for (auto constraint : constrains) {
+	
+	}
+}
+void BasicQuadMesh::AddLineConstraint(std::vector<MeshData::Domain>& domains)
+{
+	for (auto& domain : domains)
+		for (auto& lineConstraint : domain.getLineConstraints())
+			this->AddPointConstraint(domain, lineConstraint->getConstraints());
+}
+void BasicQuadMesh::ReturnToCartesian(std::vector<MeshData::Domain>& domains)
+{
 
-//------------------------------------------------ Edge Constraint --------------------------------------------------------
-MeshData::Domain::EdgeConstraint::EdgeConstraint(std::shared_ptr<MeshData::Domain::Edge> edge) : m_Edge(edge) {}
-std::shared_ptr<MeshData::Domain::Edge> MeshData::Domain::EdgeConstraint::getEdge() { return this->m_Edge; }
-void MeshData::Domain::EdgeConstraint::addRelativeConstraintLocations(double relativeConstraintLocation)
-{ this->m_RelativeConstraintLocations.push_back(relativeConstraintLocation); }
-const std::vector<double>& MeshData::Domain::EdgeConstraint::getRelativeConstraintLocations() { return this->m_RelativeConstraintLocations; }
+}
+double BasicQuadMesh::FindOptimumEdgeLength(std::vector<MeshData::Domain>& domains)
+{
+	for (auto& domain : domains)
+	{
 
-//------------------------------------------------------ Domain -----------------------------------------------------------
-void MeshData::Domain::addCorner(std::shared_ptr<Corner> corner) { this->m_Corners.push_back(corner); }
-void MeshData::Domain::addPointConstriant(std::shared_ptr<PointConstraint> pointConstraint) { this->m_PointConstraints.push_back(pointConstraint); }
-void MeshData::Domain::addLineConstraint(std::shared_ptr<LineConstraint> lineConstraint) { this->m_LineConstraints.push_back(lineConstraint); }
-void MeshData::Domain::addEdgeConstraint(std::shared_ptr<EdgeConstraint> edgeConstraint) { this->m_EdgeConstraints.push_back(edgeConstraint); }
-const std::vector<std::shared_ptr<MeshData::Domain::Corner>>& MeshData::Domain::getCorners() { return this->m_Corners; }
-const std::vector<std::shared_ptr<MeshData::Domain::PointConstraint>>& MeshData::Domain::getPointConstriants() { return this->m_PointConstraints; }
-const std::vector<std::shared_ptr<MeshData::Domain::LineConstraint>>& MeshData::Domain::getLineConstraints() { return this->m_LineConstraints; }
-const std::vector<std::shared_ptr<MeshData::Domain::EdgeConstraint>>& MeshData::Domain::getEdgeConstraints() { return this->m_EdgeConstraints; }
-std::shared_ptr<DoublyConnectedList::DCEL> MeshData::Domain::getDCEL() { return m_DCEL; }
+	}
+	return 0.0;
+}
+bool BasicQuadMesh::CheckQualityOfMesh(std::vector<MeshData::Domain>& domains)
+{
+	for (auto& domain : domains)
+	{
+
+	}
+	return true;
+}
+
+DoublyConnectedList::Vertex::Coordinates BasicQuadMesh::RayIntersection(const DoublyConnectedList::Vertex::Coordinates& origin1, const DoublyConnectedList::Vertex::Coordinates& direction1,
+	const DoublyConnectedList::Vertex::Coordinates& origin2, const DoublyConnectedList::Vertex::Coordinates& direction2)
+{
+	// normalize the direction vectors
+	auto dir1 = direction1 / (sqrt(direction1.xCoord * direction1.xCoord + direction1.yCoord * direction1.yCoord));
+	auto dir2 = direction2 / (sqrt(direction2.xCoord * direction2.xCoord + direction2.yCoord * direction2.yCoord));
+	// Caulculate the distance between the intersection point and the origin 2 by solving the intersection
+	// equation between 2 rays.
+	auto distanceBetweenTheIntersectionAndOrigin2 = (origin1.yCoord + dir1.yCoord * (origin2.xCoord - origin1.xCoord) / dir1.xCoord - origin2.yCoord) /
+		(dir2.yCoord - dir2.xCoord * dir1.yCoord / dir1.xCoord);
+
+	auto result = origin2 + dir2 * distanceBetweenTheIntersectionAndOrigin2;
+	return result;
+}
